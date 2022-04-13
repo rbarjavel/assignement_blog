3d9fa1d74b0b083cc7169bbf32c09d0b7ccd9060
Barjavel <barja@pop-os.localdomain>
2022-04-13 20:07:20 +0100

    changing route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var app = require('./route.js')
 
-var server = app.listen(process.env.PORT || 8080, () => {
+var server = app.listen(80, () => {
    var port = server.address().port
    
    console.log(`Running on port ${port}`)
